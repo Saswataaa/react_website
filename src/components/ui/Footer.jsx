7e87e0e1f809bfa5247162a7bc4a89ed55c3d105
saswata <saswata@zapscale.com>
2025-02-21 23:48:54 +0530

    feat: create footer section

--- src/components/ui/Footer.jsx
+++ src/components/ui/Footer.jsx
@@ -1,3 +1,72 @@
+import footerData from '../../api/FooterData.json'
+import {MapPin} from '@phosphor-icons/react'
+import { Phone } from '@phosphor-icons/react'
+import { EnvelopeSimple } from '@phosphor-icons/react'
+import { NavLink } from 'react-router-dom'
+
 export const Footer = ()=>{
-    return <h1>Hello this is Footer</h1>
+
+    const icons = {
+        MapPin:<MapPin size={32} />,
+        Phone:<Phone size={32} />,
+        EnvelopeSimple:<EnvelopeSimple size={32} />
+    }
+    return (
+        <footer className='footer-section'>
+            <div className='container grid grid-three-cols'>
+                {
+                    footerData.map((data,index)=>{
+                        return (
+                            <div className='footer-contact' key={index}>
+                                <div className='icon'>{icons[data.icon]}</div>
+                                <div className='footer-contact-text'>
+                                    <p>{data.title}</p>
+                                    <p>{data.details}</p>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+            <div className='copyright-area'>
+                <div className='container'>
+                    <div className='grid grid-two-cols'>
+                        <div className='copyright-text'> 
+                            <p>
+                                Copyright & copy; 2025, All Rights Reserved
+                                <NavLink to={""}>
+                                    Test
+                                </NavLink>
+                            </p>
+                        </div>
+                        <div className='footer-menu'>
+                            <ul>
+                                <li>
+                                    <NavLink to={"/"} target='_blank'>
+                                        Home
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to={""} target='_blank'>
+                                        Social
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to={""} target='_blank'>
+                                        Source Code
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to={"/contact"} target='_blank'>
+                                        Contact
+                                    </NavLink>
+                                </li>
+                                
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
 }
